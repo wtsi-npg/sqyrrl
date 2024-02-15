5db02db6ed7ba180d798e833204440b852c48536
Keith James <kdj@sanger.ac.uk>
2024-02-15 14:43:55 +0000

    Add licenses, documentation and generally tidy up

--- cmd/sqyrrl.go
+++ cmd/sqyrrl.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024. Genome Research Ltd. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package main
 
 import (
@@ -102,7 +119,7 @@ func main() {
 		Run:   startServer,
 	}
 
-	startCmd.Flags().StringVar(&serverFlags.addr, "addr", "127.0.0.1:8080",
+	startCmd.Flags().StringVar(&serverFlags.addr, "addr", "127.0.0.1:3333",
 		"Address on which to listen")
 	startCmd.Flags().StringVar(&serverFlags.envFilePath, "irods-env",
 		getIRODSEnvFilePath(),
--- internal/version.go
+++ internal/version.go
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2024. Genome Research Ltd. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package internal
 
+// Version is the version string for the sqyrrl application. It is obtained from git
+// tags and set at build time. It is printed by the --version CLI flag.
 var Version string
